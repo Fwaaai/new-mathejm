f2b0b0ed4d16e6bb08005f72622396d4c6595e44
Fwaaai <reagain.shousetu@gmail.com>
2025-10-28 14:23:56 +0100

    nvm. redo the stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/sidebar";
 import Accueil from "./pages/accueil";
 import Chap1 from "./pages/chap1";
-import { chapitre1Routes } from "./pages/chapitre-1/routes";
 import NotFound from "./pages/NotFound";
 
 export default function App(): React.JSX.Element {
@@ -14,8 +13,6 @@ export default function App(): React.JSX.Element {
         <Routes>
           <Route path="/" element={<Accueil />} />
           <Route path="/chapitres/chapitre-1/*" element={<Chap1 />}>
-            {/* Nested routes will be imported from chapitre-1/routes.tsx */}
-            {chapitre1Routes}
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
